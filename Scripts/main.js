cc65fd4e8fa139591a338a59729ad8269f0e7352
Isabella Linares <isabellalinarescamacho0517@gmail.com>
2020-09-22 23:24:04 -0500

    Se empezó a realizar el slider para la imagen de la sección interacción

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,5 @@
+
+//Carrousel
 const carrouselRight = document.querySelector('.carrousel__right');
 const carrouselStripe = document.querySelector('.carrousel__stripe');
 let current = 0;
@@ -10,3 +12,14 @@ carrouselRight.addEventListener('click',function() {
     const width = carrouselRight.clientWidth;
     carrouselStripe.style.transform = 'translate(-' + (width * current) + 'px, 0px)';  
 });
+
+
+//Slider
+const slider = document.querySelector('.interaction__input');
+const img = document.querySelector('.interaction__slider img:last-child');
+
+function handleSlider () {
+  
+  img.style.width = (slider.value * 100) + '%';
+}
+slider.addEventListener('input', handleSlider);
