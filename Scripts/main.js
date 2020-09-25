21db6a0ab0a70cd3af12f2d4a0e1f0fd7f7a3989
Isabella Linares <isabellalinarescamacho0517@gmail.com>
2020-09-25 15:58:09 -0500

    Se realizó la interacción del banner para que cambie de foto

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,22 +5,22 @@ const carrouselLeft = document.querySelector('.carrousel__left');
 const carrouselStripe = document.querySelector('.carrousel__stripe');
 let current = 0;
 
-carrouselRight.addEventListener('click',function() {
-    current++;
-    if(current >= carrouselStripe.children.length) {
-        current = 0;
-    }
-    const width = carrouselStripe.clientWidth;
-    carrouselStripe.style.transform = 'translate(-' + (width * current) + 'px, 0px)';  
-});
-
-carrouselLeft.addEventListener('click',function() {
-  current--;
-  if(current<=0){
+carrouselRight.addEventListener('click', function () {
+  current++;
+  if (current >= carrouselStripe.children.length) {
     current = 0;
   }
   const width = carrouselStripe.clientWidth;
-  carrouselStripe.style.transform = 'translate(+' + (width * current) + 'px, 0px)';  
+  carrouselStripe.style.transform = 'translate(-' + (width * current) + 'px, 0px)';
+});
+
+carrouselLeft.addEventListener('click', function () {
+  current--;
+  if (current <= 0) {
+    current = 0;
+  }
+  const width = carrouselStripe.clientWidth;
+  carrouselStripe.style.transform = 'translate(+' + (width * current) + 'px, 0px)';
 });
 
 
@@ -29,8 +29,23 @@ carrouselLeft.addEventListener('click',function() {
 const slider = document.querySelector('.interaction__input');
 const img = document.querySelector('.interaction__slider img:last-child');
 
-function handleSlider () {
-  
+function handleSlider() {
+
   img.style.width = (slider.value * 100) + '%';
 }
 slider.addEventListener('input', handleSlider);
+
+//Banner
+const banner = document.querySelector('.banner');
+var image1 ='url(../Images/WomanBanner1.jpg)';
+var image2 ='url(../Images/WomanBanner2.jpg)';
+window.setInterval(function(){
+ 
+  if(banner.classList.contains('bannerImg2')){
+    banner.classList.remove('bannerImg2');
+  }else{
+    banner.classList.add('bannerImg2');
+  }
+},5000);
+
+
